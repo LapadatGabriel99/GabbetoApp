ba4f7ab4f4f1941f827ef4b990196f4cca4995a8
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-05-26 22:58:51 +0300

    Added dummy data to the friend request design models.

--- Gabbeto.Word.Core/ViewModel/Friends/FriendRequest/Design/FriendRequestListDesignModel.cs
+++ Gabbeto.Word.Core/ViewModel/Friends/FriendRequest/Design/FriendRequestListDesignModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.ObjectModel;
 
 namespace Fasseto.Word.Core
 {
@@ -25,7 +26,90 @@ namespace Fasseto.Word.Core
         /// </summary>
         public FriendRequestListDesignModel()
         {
+            Items = new ObservableCollection<FriendRequestListItemViewModel>
+            {
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "3099c5",
+                    NewContentAvailable = true
+                },
 
+                new FriendRequestListItemViewModel
+                {
+                    Name = "BossBaros",
+                    Initials = "BB",
+                    Message = "Sa traiasca nasu",
+                    ProfilePictureRGB = "ff0000"
+                },
+
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "0000ff"
+                },
+
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "fe4503",
+                    IsSelected = true
+                },
+
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "00d405"
+                },
+
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "00d455"
+                },
+
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "00d450"
+                },
+
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "ff55ff"
+                },
+
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "ff0045"
+                },
+
+                new FriendRequestListItemViewModel
+                {
+                    Name = "Gabi",
+                    Initials = "GM",
+                    Message = "Heeeyaaaaaaaaaa",
+                    ProfilePictureRGB = "00d045"
+                }
+            };
         }
 
         #endregion
