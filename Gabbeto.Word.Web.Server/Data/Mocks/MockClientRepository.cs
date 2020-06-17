f7d4c1a7a71902b7adfe04bfa1ed0a537e2a374b
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-06-18 01:45:46 +0300

    Added the repositories for the web socket data models.

---
+++ Gabbeto.Word.Web.Server/Data/Mocks/MockClientRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gabbetto.Word.Web.Server
+{
+    /// <summary>
+    /// The repository that handles the web socket clients
+    /// </summary>
+    public class MockClientRepository : IClientRepository
+    {
+        #region Interface Implementation
+
+        /// <summary>
+        /// Adds a client to the database
+        /// </summary>
+        /// <param name="client"></param>
+        public void AddClient(WebSocketClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a client from the database
+        /// </summary>
+        /// <returns></returns>
+        public WebSocketClient GetClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes a client from the database
+        /// </summary>
+        /// <param name="client"></param>
+        public void RemoveClient(WebSocketClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates a client in the database
+        /// </summary>
+        /// <param name="client"></param>
+        public void UpdateClient(WebSocketClient client)
+        {
+            throw new NotImplementedException();
+        } 
+
+        #endregion
+    }
+}
---
+++ Gabbeto.Word.Web.Server/Data/Mocks/MockGroupRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gabbetto.Word.Web.Server
+{
+    /// <summary>
+    /// The repository that handles the web socket groups
+    /// </summary>
+    public class MockGroupRepository : IGroupRepository
+    {
+        #region Interface Implementation
+
+        /// <summary>
+        /// Adds a group to the database
+        /// </summary>
+        /// <param name="group"></param>
+        public void AddGroup(WebSocketGroup group)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a group from the database
+        /// </summary>
+        /// <returns></returns>
+        public WebSocketGroup GetGroup()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes a group from the database
+        /// </summary>
+        /// <param name="group"></param>
+        public void RemoveGroup(WebSocketGroup group)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates a group in the database
+        /// </summary>
+        /// <param name="group"></param>
+        public void UpdateGroup(WebSocketGroup group)
+        {
+            throw new NotImplementedException();
+        } 
+
+        #endregion
+    }
+}
