6109e8179f2af6627f302fb94685ecb667e80ec0
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-06-17 12:29:42 +0300

    Added the web socket client model.

---
+++ Gabbeto.Word.Web.Server/Data/Models/WebSocketClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gabbetto.Word.Web.Server
+{
+    public class WebSocketClient
+    {
+    }
+}
