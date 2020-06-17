055d9097e0b394ed09c29f11339417f6a832e7d2
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-06-17 12:24:53 +0300

    Added a few indicative methods to the friends hub.

--- Gabbeto.Word.Web.Server/Hubs/FriendsHub.cs
+++ Gabbeto.Word.Web.Server/Hubs/FriendsHub.cs
@@ -11,5 +11,46 @@ namespace Gabbetto.Word.Web.Server
     /// </summary>
     public class FriendsHub : Hub
     {
+        /// <summary>
+        /// The method that is called when a client connects to the this hub
+        /// </summary>
+        /// <returns></returns>
+        public override Task OnConnectedAsync()
+        {            
+            // Call the base method
+            return base.OnConnectedAsync();
+        }
+
+        /// <summary>
+        /// The method that is called when a client disconnects from this hub
+        /// </summary>
+        /// <returns></returns>
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+
+            // Call the base method
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        /// <summary>
+        /// Sends a friend request notification to the specified user
+        /// </summary>
+        /// <param name="user">The specified user</param>        
+        /// <returns></returns>
+        public Task SendFriendRequest(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sends a friend request response notification to the specified user
+        /// </summary>
+        /// <param name="user">The specified user</param>        
+        /// <param name="response">The friend request response</param>
+        /// <returns></returns>
+        public Task SendFriendRequestResponse(string user, string response)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
