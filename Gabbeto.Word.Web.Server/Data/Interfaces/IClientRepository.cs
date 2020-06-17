c715f75967b06444a63f3e5cc8e0e28ecae6737c
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-06-18 01:26:13 +0300

    Added the interfaces for the repositories that handle the web socket data models.

---
+++ Gabbeto.Word.Web.Server/Data/Interfaces/IClientRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gabbetto.Word.Web.Server
+{
+    /// <summary>
+    /// The interface for the repository that handles web socket clients
+    /// </summary>
+    public interface IClientRepository
+    {
+        /// <summary>
+        /// Adds a client to the database
+        /// </summary>
+        /// <param name="client"></param>
+        void AddClient(WebSocketClient client);
+
+        /// <summary>
+        /// Updates a client in the database
+        /// </summary>
+        /// <param name="client"></param>
+        void UpdateClient(WebSocketClient client);
+
+        /// <summary>
+        /// Removes a client from the database
+        /// </summary>
+        /// <param name="client"></param>
+        void RemoveClient(WebSocketClient client);
+
+        /// <summary>
+        /// Gets a client from the database
+        /// </summary>
+        /// <returns></returns>
+        WebSocketClient GetClient();
+    }
+}
---
+++ Gabbeto.Word.Web.Server/Data/Interfaces/IGroupRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gabbetto.Word.Web.Server
+{
+    /// <summary>
+    /// The interface for the repository that handles web socket groups
+    /// </summary>
+    public interface IGroupRepository
+    {
+        /// <summary>
+        /// Adds a group to the database
+        /// </summary>
+        /// <param name="group"></param>
+        void AddGroup(WebSocketGroup group);
+
+        /// <summary>
+        /// Updates a group in the database
+        /// </summary>
+        /// <param name="group"></param>
+        void UpdateGroup(WebSocketGroup group);
+
+        /// <summary>
+        /// Removes a group from the database
+        /// </summary>
+        /// <param name="group"></param>
+        void RemoveGroup(WebSocketGroup group);
+
+        /// <summary>
+        /// Gets a group from the database
+        /// </summary>
+        /// <returns></returns>
+        WebSocketGroup GetGroup();
+    }
+}
