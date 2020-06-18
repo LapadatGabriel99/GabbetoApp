881bbcf7437b806a89c6386c67601aa920cc79c6
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-06-18 15:35:20 +0300

    Made the interface for the web socket manager.

---
+++ Gabbeto.Word.Web.Server/Data/Interfaces/IConnetionManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gabbetto.Word.Web.Server.Data.Interfaces
+{
+    /// <summary>
+    /// The interface for the connection manager that handles web socket related data
+    /// </summary>
+    public interface IConnetionManager
+    {
+        /// <summary>
+        /// Registers a connection of a client
+        /// </summary>
+        /// <param name="client">The specified client</param>
+        /// <param name="connection">The specified connection</param>
+        /// <returns></returns>
+        Task AddConnetion(WebSocketClient client, WebSocketConnection connection);
+
+        /// <summary>
+        /// Removes a specific connection
+        /// </summary>
+        /// <param name="connection">The specified connection</param>
+        /// <returns></returns>
+        Task RemoveConnetion(WebSocketConnection connection);
+
+        /// <summary>
+        /// Gets the connections of a specified client
+        /// </summary>
+        /// <param name="client">The specified client</param>
+        /// <returns></returns>
+        ICollection<WebSocketConnection> GetConnections(WebSocketClient client);
+
+        /// <summary>
+        /// Gets the current on line users
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<WebSocketClient> OnlineUsers();
+    }
+}
