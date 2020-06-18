355059c3643ec93ac86f700b04dceb7d0ef983e9
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-06-18 14:26:34 +0300

    Added a new migration and updated the database in order to have the new setup.

--- Gabbeto.Word.Web.Server/Migrations/20200618112429_AddedTheWebSocketClientDataModel.Designer.cs
+++ Gabbeto.Word.Web.Server/Migrations/20200618112429_AddedTheWebSocketClientDataModel.Designer.cs
@@ -4,14 +4,16 @@ using Gabbetto.Word.Web.Server;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Gabbetto.Word.Web.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200618112429_AddedTheWebSocketClientDataModel")]
+    partial class AddedTheWebSocketClientDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Gabbeto.Word.Web.Server/Migrations/20200618112429_AddedTheWebSocketClientDataModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gabbetto.Word.Web.Server.Migrations
+{
+    public partial class AddedTheWebSocketClientDataModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConnectionId",
+                table: "Clients");
+
+            migrationBuilder.CreateTable(
+                name: "Connections",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    ConnectionId = table.Column<string>(nullable: false),
+                    ClientId = table.Column<string>(nullable: true),
+                    Connected = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Connections_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_ClientId",
+                table: "Connections",
+                column: "ClientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connections");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConnectionId",
+                table: "Clients",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
